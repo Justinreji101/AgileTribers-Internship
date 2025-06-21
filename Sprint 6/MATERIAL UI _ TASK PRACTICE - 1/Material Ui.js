7041e4bd8142d0b979e54cbd76b85bc4bfee4a4d
Justin Reji <justinreji101@gmail.com>
2025-06-21 13:46:55 +0530

    Delete Sprint 6 directory

--- Sprint 6/MATERIAL UI _ TASK PRACTICE - 1/Material Ui.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('addTaskBtn').addEventListener('click', function() {
-            const form = document.getElementById('addTaskForm');
-            form.style.display = form.style.display === 'none' ? 'block' : 'none';
-        });
